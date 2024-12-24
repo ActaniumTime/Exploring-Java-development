e6288e81d99fa125825d804761dcc780a16c44fe
Mashkov Volodymyr <103945286+ActaniumTime@users.noreply.github.com>
2024-12-24 12:21:22 +0200

    Task 10 is done

---
+++ PR 10/pr10/src/com/classes/MyThread.java
@@ -0,0 +1,36 @@
+package com.classes;
+
+public class MyThread extends Thread{
+//    public MyThread(String name){
+//        super(name);
+//    }
+//
+//    @Override
+//    public void run(){
+//        try {
+//            System.out.println("Stream  " + getName() + "has began");
+//            Thread.sleep(1000);
+//            System.out.println("Thread" + getName() + "has been stopped");
+//        }
+//        catch (InterruptedException e){
+//            System.out.println("Thread " + getName() + "has been interrupted");
+//        }
+//    }
+
+
+    //task 2
+
+//    Counter counter;
+//
+//    public MyThread(Counter counter){
+//        this.counter = counter;
+//    }
+//
+//    @Override
+//    public void run(){
+//        for (int i = 0; i <1000; i++){
+//            counter.increment();
+//        }
+//    }
+
+}
